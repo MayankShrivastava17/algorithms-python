c7f2d755aecb35c929c4675056458de9ba738842
Mayank Shrivastava <mayanksri910@gmail.com>
2021-10-04 00:00:17 +0530

    Update mergeSort.py

--- sorting/mergeSort.py
+++ sorting/mergeSort.py
@@ -1,23 +1,11 @@
-
 def mergeSort(arr):
 	if len(arr) > 1:
-
-	
 		mid = len(arr)//2
-
-	
 		L = arr[:mid]
-
-	
 		R = arr[mid:]
-
 		mergeSort(L)
-
-	
 		mergeSort(R)
-
 		i = j = k = 0
-
 		while i < len(L) and j < len(R):
 			if L[i] < R[j]:
 				arr[k] = L[i]
@@ -26,22 +14,21 @@ def mergeSort(arr):
 				arr[k] = R[j]
 				j += 1
 			k += 1
-
 		while i < len(L):
 			arr[k] = L[i]
 			i += 1
 			k += 1
-
 		while j < len(R):
 			arr[k] = R[j]
 			j += 1
 			k += 1
 
 
-
-
 n = int(input("Enter the size of array : "))
 
 arr = list(map(int, input("Enter the array elements :\n").strip().split()))[:n]
+print("Array Before")
+print(arr)
 mergeSort(arr)
+print("Array After")
 print(arr)
