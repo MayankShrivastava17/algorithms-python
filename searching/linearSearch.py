b7c01b45b77d213cd68e1b9b9b7bf6631df55505
Mayank Shrivastava <mayanksri910@gmail.com>
2021-10-04 17:46:18 +0530

    Update linearSearch.py

--- searching/linearSearch.py
+++ searching/linearSearch.py
@@ -3,10 +3,13 @@ def search(arr, n, x):
 		if (arr[i] == x):
 			return i
 	return -1
+
 n = int(input("Enter the size of array : "))
 arr = list(map(int, input("Enter the array elements :\n").strip().split()))[:n]
 x=int(input("Enter the element to search : "))
+
 result = search(arr, n, x)
+
 if(result == -1):
 	print("Element is not present in array")
 else:
