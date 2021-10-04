e86876f54ad26107623591438668b204119baa24
scarlledo <tapadyutisagar@gmail.com>
2021-10-04 17:43:54 +0530

    Added Linear Search

---
+++ searching/linearSearch.py
@@ -0,0 +1,13 @@
+def search(arr, n, x):
+	for i in range(0, n):
+		if (arr[i] == x):
+			return i
+	return -1
+n = int(input("Enter the size of array : "))
+arr = list(map(int, input("Enter the array elements :\n").strip().split()))[:n]
+x=int(input("Enter the element to search : "))
+result = search(arr, n, x)
+if(result == -1):
+	print("Element is not present in array")
+else:
+	print("Element is present at index", result)
