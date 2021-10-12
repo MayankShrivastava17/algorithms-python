bec67a51772d9b32c556259c33fc4174e2979aa3
kaverichavan <62557306+kaverichavan@users.noreply.github.com>
2021-10-12 21:00:25 +0530

    Add files via upload

---
+++ searching/Next palindrome number.py
@@ -0,0 +1,16 @@
+def reverse(num):
+    reverse= 0
+    while num:
+        reverse= reverse*10 + num%10
+        num= num//10
+    return reverse
+
+num= int(input("Enter any number :- "))
+if num==reverse(num):
+    print ("Already palindrome.")
+else:
+    while True:
+        num+= 1
+        if num==reverse(num):
+            print ("Next palindrome is : %s"%num)
+            break
