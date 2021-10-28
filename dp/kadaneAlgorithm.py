31c5421b1decfd1ec60839825bda64f690f11379
Mayank Shrivastava <mayanksri910@gmail.com>
2021-10-29 00:02:07 +0530

    Update and rename Kadane's algo.py to kadaneAlgorithm.py

--- dp/kadaneAlgorithm.py
+++ dp/kadaneAlgorithm.py
@@ -1,14 +1,16 @@
+import sys
+
 def maxSubArraySum(a,size):
-      
-    max_so_far = -maxint - 1
+    max_so_far = -sys.maxsize - 1
     max_ending_here = 0
-      
     for i in range(0, size):
         max_ending_here = max_ending_here + a[i]
         if (max_so_far < max_ending_here):
             max_so_far = max_ending_here
- 
         if max_ending_here < 0:
             max_ending_here = 0  
     return max_so_far
-  
+    
+n = int(input("Enter the size of array : "))
+arr = list(map(int, input("Enter the array elements :\n").strip().split()))[:n]
+print (f'Max Sub Array Sum using Kadane\'s Algorithm is {maxSubArraySum(arr, n)}')
